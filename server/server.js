c06286291580f4c40b3102de89916266bc431e85
DorothyHYY <Doro_hyy@163.com>
2018-03-19 04:09:46 -0400

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -14,11 +14,21 @@ app.use(express.static(publicPath));  // use a middleware
 io.on('connection',(socket)=>{
     console.log('New user connected');
 
+    socket.emit('newEmail',{
+        from: 'mike@example.com',
+        text: 'Hey, What is going on.',
+        createAt: 123
+    });
+
+    socket.on('createEmail', (newEmail)=>{
+        console.log('createEmail', newEmail);
+    });
+
     socket.on('disconnect',()=>{
         console.log('User was disconnected');
     });
 });
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Server is up on ${port}`);
 });
